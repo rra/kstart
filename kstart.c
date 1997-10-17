7ba0087f9cd31d691b4ec30d5e0b8ba72ff93729
Booker Bense <bbense@stanford.edu>
1997-10-17 22:45:50 +0000

    - Added long term ticket updating

--- kstart.c
+++ kstart.c
@@ -72,7 +72,8 @@ static char rcsid_kinit_c[] =
 #define	LEN	MAXHOSTNAMELEN
 #endif	/* defined(ultrix) || defined(sun) */
 #endif	/* BSD42 */
-
+/* Solaris 2.5.x groks this */ 
+#include <string.h> 
 #if !defined(LEN) 
 #define LEN 255
 #endif
@@ -96,6 +97,8 @@ extern int krb_debug;
 
 char   *progname;
 
+void usage() ; 
+
 void
 get_input(s, size, stream)
 char *s;
@@ -106,7 +109,7 @@ FILE *stream;
 
 	if (fgets(s, size, stream) == NULL)
 	  exit(1);
-	if ( (p = strchr(s, '\n')) != NULL)
+	if ( (p = (char *)strchr(s, '\n')) != NULL)
 		*p = '\0';
 }
 
@@ -114,224 +117,285 @@ int main(argc, argv)
     int     argc;
     char   *argv[];
 {
-    char    aname[ANAME_SZ];
-    char    inst[INST_SZ];
-    char    realm[REALM_SZ];
-    char    sname[SNAME_SZ];
-    char    sinst[INST_SZ];
-    char    srvtab[MAXPATHLEN + 1] ; 
-    char    buf[LEN];
-    char   *username = NULL;
-    int     iflag, rflag, vflag, lflag, lifetime, k_errno;
-    int     tflag;
-    int     sflag;
-    int     qflag;
-    int     nflag;
-    int     fflag;
-    char    *the_kinit_prog;
-    int     c;
-    register char *cp;
-    register i;
+  char    aname[ANAME_SZ];
+  char    inst[INST_SZ];
+  char    realm[REALM_SZ];
+  char    sname[SNAME_SZ];
+  char    sinst[INST_SZ];
+  char    srvtab[MAXPATHLEN + 1] ; 
+  char    ticket_file[MAXPATHLEN + 1] ; 
+  char    buf[LEN];
+  char   *username = NULL;
+  int     iflag, rflag, vflag, lflag, lifetime, k_errno;
+  int     tflag;
+  int     sflag;
+  int     qflag;
+  int     nflag;
+  int     fflag;
+  int     kflag; 
+  int     keep_ticket; 
+  char    *the_kinit_prog;
+  int     c;
+  register char *cp;
+  register i;
 
-    *inst = *realm = *sname = *sinst = '\0';
-    iflag = rflag = vflag = lflag = 0;
-    tflag = 0;
-    sflag = 0;
-    qflag = 0;
-    nflag = 0;
-    fflag = 0; 
-    the_kinit_prog = NULL;
-    lifetime = LIFE;
-    progname = (cp = strrchr(*argv, '/')) ? cp + 1 : *argv;
+  *inst = *realm = *sname = *sinst = '\0';
+  iflag = rflag = vflag = lflag = 0;
+  tflag = 0;
+  sflag = 0;
+  qflag = 0;
+  nflag = 0;
+  fflag = 0; 
+  kflag = 0; 
+  keep_ticket = 0 ; 
+  the_kinit_prog = NULL;
+  lifetime = LIFE;
+  progname = (cp = (char *)strrchr(*argv, '/')) ? cp + 1 : *argv;
 
-    opterr=1;
+  opterr=1;
 
-    while ((c = getopt(argc,argv,"dspqtnvu:i:r:S:I:l:f:h:")) != EOF) 
-      switch (c) {
-      case 'l': if ( (lifetime=atoi(optarg)) <0) usage();  break;
-      case 'p':
-      case 's': 
-	++sflag; 
-	if ( fflag != 0 ) { 
-	  printf("%s: cannot use both -s and -f flags\n",
-		 progname);
-	  usage;
-	}
-	break;
-      case 'q': ++qflag;                                   break;
-      case 't': ++tflag;                                   break;
-      case 'n': ++nflag;                                   break;
-      case 'd': ++krb_debug;                               break;
-      case 'u': username = optarg;                         break;
-      case 'f': 
-	++fflag ;
-	if (strlen(optarg) < sizeof(srvtab)) {
-	  strcpy(srvtab, optarg);
-	} else {
-	  printf("%s: srvtab '%s' too long (%d max)\n",
-		 progname, optarg, sizeof(srvtab));
-	  usage;
-	}
-	if ( sflag != 0 ) { 
-	  printf("%s: cannot use both -s and -f flags\n",
-		 progname);
-	  usage;
-	} 
-	break;
-      case 'i': 
-	if (strlen(optarg) < sizeof(inst)) {
-	  strcpy(inst, optarg);
-	} else {
-	  printf("%s: instance '%s' too long (%d max)\n",
-		 progname, optarg, sizeof(inst));
-	  usage;
-	}
-	break;
-      case 'r':
-	if (strlen(optarg) < sizeof(realm)) {
-	  strcpy(realm, optarg);
-	} else {
-	  printf("%s: realm '%s' too long (%d max)\n",
-		 progname, optarg, sizeof(realm));
-	  usage;
-	}
-	break;
-      case 'S':
-	if (strlen(optarg) < sizeof(sname)) {
-	  strcpy(sname, optarg);
-	} else {
-	  printf("%s: service name '%s' too long (%d max)\n",
-		 progname, optarg, sizeof(sname));
-	  usage;
-	}
-	break;
-      case 'I':
-	if (strlen(optarg) < sizeof(sinst)) {
-	  strcpy(sinst, optarg);
-	} else {
-	  printf("%s: service instance '%s' too long (%d max)\n",
-		 progname, optarg, sizeof(sinst));
-	  usage;
-	}
-	break;
-      default: usage();
+  while ((c = getopt(argc,argv,"dspqtnvu:i:r:S:I:l:f:K:k:")) != EOF) 
+    switch (c) {
+    case 'l': if ( (lifetime=atoi(optarg)) <0) usage();  break;
+    case 'p':
+    case 's': 
+      ++sflag; 
+      if ( fflag != 0 ) { 
+	printf("%s: cannot use both -s and -f flags\n",
+	       progname);
+	usage;
       }
-	argv = &argv[optind];
-	if (*argv) {
-	  if (username) {
-	    usage();
-	  } else {
-	    username = *argv;
-	  }
-	}
+      break;
+    case 'q': ++qflag;                                   break;
+    case 't': ++tflag;                                   break;
+    case 'v': ++vflag;                                   break;
+    case 'n': ++nflag;                                   break;
+    case 'd': ++krb_debug;                               break;
+    case 'u': username = optarg;                         break;
+      /*Stayin' Alive, uh, uh, uh, stayin' aliveeeee ! */ 
+    case 'K': keep_ticket = atoi(optarg);                break;
+    case 'k': 
+      ++kflag ;
+      if (strlen(optarg) < sizeof(ticket_file)) {
+	strcpy(ticket_file, optarg);
+      } else {
+	printf("%s: ticket file '%s' too long (%d max)\n",
+	       progname, optarg, sizeof(ticket_file));
+	usage;
+      }
+      break ; 
+    case 'f': 
+      ++fflag ;
+      if (strlen(optarg) < sizeof(srvtab)) {
+	strcpy(srvtab, optarg);
+      } else {
+	printf("%s: srvtab '%s' too long (%d max)\n",
+	       progname, optarg, sizeof(srvtab));
+	usage;
+      }
+      if ( sflag != 0 ) { 
+	printf("%s: cannot use both -s and -f flags\n",
+	       progname);
+	usage;
+      } 
+      break;
+    case 'i': 
+      if (strlen(optarg) < sizeof(inst)) {
+	strcpy(inst, optarg);
+      } else {
+	printf("%s: instance '%s' too long (%d max)\n",
+	       progname, optarg, sizeof(inst));
+	usage;
+      }
+      break;
+    case 'r':
+      if (strlen(optarg) < sizeof(realm)) {
+	strcpy(realm, optarg);
+      } else {
+	printf("%s: realm '%s' too long (%d max)\n",
+	       progname, optarg, sizeof(realm));
+	usage;
+      }
+      break;
+    case 'S':
+      if (strlen(optarg) < sizeof(sname)) {
+	strcpy(sname, optarg);
+      } else {
+	printf("%s: service name '%s' too long (%d max)\n",
+	       progname, optarg, sizeof(sname));
+	usage;
+      }
+      break;
+    case 'I':
+      if (strlen(optarg) < sizeof(sinst)) {
+	strcpy(sinst, optarg);
+      } else {
+	printf("%s: service instance '%s' too long (%d max)\n",
+	       progname, optarg, sizeof(sinst));
+	usage;
+      }
+      break;
+    default: usage();
+    }
+  argv = &argv[optind];
+  if (*argv) {
+    if (username) {
+      usage();
+    } else {
+      username = *argv;
+    }
+  }
+  if ( keep_ticket > 0 && fflag == 0 ) { 
+    printf("%s: Keep ticket option -K requires -f option\n",progname) ; 
+    usage; 
+  }
+  if ( keep_ticket > lifetime ) { 
+    printf("%s: keep_ticket %d > lifetime %d\n",progname,keep_ticket,lifetime); 
+    usage; 
+  }
+  the_kinit_prog =  (char *)getenv("KINIT_PROG");
 
-	the_kinit_prog =  (char *)getenv("KINIT_PROG");
+  if (the_kinit_prog==NULL) {
+    the_kinit_prog = AKLOG_PROGRAM;
+  } else {
+    tflag=1;
+  }
 
-	if (the_kinit_prog==NULL) {
-	  the_kinit_prog = AKLOG_PROGRAM;
-	} else {
-	  tflag=1;
-	}
+  if (username==NULL) {
+    struct passwd *pw;
+    pw = getpwuid(getuid());
+    if (pw) { username = pw->pw_name; }
+  }
+  /* Set ticket cache */ 
+  if ( kflag ) { 
+    krb_set_tkt_string(ticket_file); 
+  }
+  if ( keep_ticket ) { 
+    if ( ! vflag ) qflag++ ;
+  }
 
-	if (username==NULL) {
-	  struct passwd *pw;
-	  pw = getpwuid(getuid());
-	  if (pw) { username = pw->pw_name; }
-	}
-	if (username &&
-	    (k_errno = kname_parse(aname, inst, realm, username))
-	    != KSUCCESS) {
-	  fprintf(stderr, "%s: %s\n", progname, krb_err_txt[k_errno]);
-	  iflag = rflag = 1;
-	  username = NULL;
-	}
-	if (k_gethostname(buf, LEN)) {
-	  fprintf(stderr, "%s: k_gethostname failed\n", progname);
-	  exit(1);
-	}
-	if (!qflag) {
-	  char *s=(char *)getenv("SESSION_ENCRYPTED"); 
-	  char *w=(char *)getenv("SESSION_CHECK");
-	  if (s==NULL) {
-	    if(w) {
-	      if (strcmp(w,"warn")==NULL) {
-		fprintf(stderr,"warning: session may not be encrypted\n");
-	      } else if (strcmp(w,"error")==NULL) {
-		fprintf(stderr,"error: session is not encrypted!\n");
-		exit(0);
-	      }
-	    }
-	  } else {
-	    fprintf(stderr,"session is encrypted with %s\n",s);
-	  }
-	}
-	if (!qflag) printf("%s (%s)\n", ORGANIZATION, buf);
-	if (username && !qflag) {
-	  printf("Kerberos Initialization for \"%s", aname);
-	  if (*inst)
-	    printf(".%s", inst);
-	  if (*realm)
-	    printf("@%s", realm);
-	  printf("\"");
-	  if (*sname) {
-	    printf(" for service \"%s", sname);
-	    if (*sinst)
-	      printf(".%s", sinst);
-	    printf("\"");
-	  }
-	  printf("\n");
-	}
+  if (username &&
+      (k_errno = kname_parse(aname, inst, realm, username))
+      != KSUCCESS) {
+    fprintf(stderr, "%s: %s\n", progname, krb_err_txt[k_errno]);
+    iflag = rflag = 1;
+    username = NULL;
+  }
+  if (k_gethostname(buf, LEN)) {
+    fprintf(stderr, "%s: k_gethostname failed\n", progname);
+    exit(1);
+  }
 
-	if (lifetime < 5)
-	  lifetime = 1;
-	else
+  if (!qflag) {
+    char *s=(char *)getenv("SESSION_ENCRYPTED"); 
+    char *w=(char *)getenv("SESSION_CHECK");
+    if (s==NULL) {
+      if(w) {
+	if (strcmp(w,"warn")== 0 ) {
+	  fprintf(stderr,"warning: session may not be encrypted\n");
+	} else if (strcmp(w,"error")== 0) {
+	  fprintf(stderr,"error: session is not encrypted!\n");
+	  exit(0);
+	}
+      }
+    } else {
+      fprintf(stderr,"session is encrypted with %s\n",s);
+    }
+  }
+  if (!qflag) printf("%s (%s)\n", ORGANIZATION, buf);
+  if (username && !qflag) {
+    printf("Kerberos Initialization for \"%s", aname);
+    if (*inst)
+      printf(".%s", inst);
+    if (*realm)
+      printf("@%s", realm);
+    printf("\"");
+    if (*sname) {
+      printf(" for service \"%s", sname);
+      if (*sinst)
+	printf(".%s", sinst);
+      printf("\"");
+    }
+    printf("\n");
+  }
+
+  if (lifetime < 5)
+    lifetime = 1;
+  else
 #ifdef SHORT_LIFETIME
-	  lifetime /= 5;
+    lifetime /= 5;
 #else
-	lifetime = krb_time_to_life(0,lifetime*60);
+  lifetime = krb_time_to_life(0,lifetime*60);
 #endif
-	/* This should be changed if the maximum ticket lifetime */
-	/* changes */
-	if (lifetime > 255)
-	  lifetime = 255;
+  /* This should be changed if the maximum ticket lifetime */
+  /* changes */
+  if (lifetime > 255)
+    lifetime = 255;
 
-	if (!*realm && krb_get_lrealm(realm, 1)) {
-	  fprintf(stderr, "%s: krb_get_lrealm failed\n", progname);
-	  exit(1);
-	}
-	if (!*sname)
-	  strcpy(sname, "krbtgt");
-	if (!*sinst)
-	  strcpy(sinst, realm);
-	if (sflag) { 
-	  char pp[132];
-	  if (! qflag ) printf("Password: "); 
-	  get_input(pp,sizeof(pp), stdin);
-	  k_errno = krb_get_pw_in_tkt(aname, inst, realm, sname, sinst,
-				      lifetime, pp);
-	} else if ( fflag ) {  
-	  k_errno = krb_get_svc_in_tkt(aname , inst, realm,
-				       sname, realm, lifetime, srvtab);
-	} else { 
-	  k_errno = krb_get_pw_in_tkt(aname, inst, realm, sname, sinst,
-				      lifetime, 0);
-	}
-	if (vflag) {
-	  printf("Kerberos realm %s:\n", realm);
-	  printf("%s\n", krb_err_txt[k_errno]);
-	} else if (k_errno) {
-	  fprintf(stderr, "%s: %s\n", progname, krb_err_txt[k_errno]);
-	  exit(1);
-	}
+  if (!*realm && krb_get_lrealm(realm, 1)) {
+    fprintf(stderr, "%s: krb_get_lrealm failed\n", progname);
+    exit(1);
+  }
+  if (!*sname)
+    strcpy(sname, "krbtgt");
+  if (!*sinst)
+    strcpy(sinst, realm);
+KEEP_ALIVE: 
+  if (sflag) { 
+    char pp[132];
+    if (! qflag ) printf("Password: "); 
+    get_input(pp,sizeof(pp), stdin);
+    k_errno = krb_get_pw_in_tkt(aname, inst, realm, sname, sinst,
+				lifetime, pp);
+  } else if ( fflag ) {  
+    k_errno = krb_get_svc_in_tkt(aname , inst, realm,
+				 sname, realm, lifetime, srvtab);
+  } else { 
+    k_errno = krb_get_pw_in_tkt(aname, inst, realm, sname, sinst,
+				lifetime, 0);
+  }
+  if (vflag) {
+    printf("Kerberos realm %s:\n", realm);
+    printf("%s\n", krb_err_txt[k_errno]);
+  } else if (k_errno) {
+    fprintf(stderr, "%s: %s\n", progname, krb_err_txt[k_errno]);
+    exit(1);
+  }
 #ifdef DO_AKLOG
-	if (tflag && !nflag) {
-	  if (! access( the_kinit_prog, X_OK)) 
-	    (void) system(the_kinit_prog);
-	}
+  if (tflag && !nflag) {
+    if (! access( the_kinit_prog, X_OK)) 
+      (void) system(the_kinit_prog);
+  }
 #endif
-	return 0;
+  if  ( keep_ticket > 0 ) { 
+    for(;;) { 
+      sleep(keep_ticket*60) ; 
+      if ( ticket_expired(sname,sinst,realm,keep_ticket) ) { 
+	goto KEEP_ALIVE ; 
       }
+    }
 
+  } else { 
+    return 0;
+  }
+}
+
+/* Stolen from appl/bsd/rlogin.c */ 
+int 
+ticket_expired(char *service,char *inst, char *realm, int check ) { 
+  CREDENTIALS cr;
+  int rem = KSUCCESS; 
+  rem = krb_get_cred(service, inst, realm, &cr);
+  if (rem == KSUCCESS
+              && ((cr.issue_date + ((unsigned char) cr.lifetime) * 5 * 60 )
+                  < ( time (0) + 60*check )))
+    rem = RD_AP_EXP;
+
+  return rem ; 
+
+} 
+
+void
 usage()
 {
     fprintf(stderr, "Usage: %s [options] [name]\n", progname);
@@ -342,6 +406,8 @@ usage()
     fprintf(stderr,"   -r <service realm>\n");
     fprintf(stderr,"   -v[erbose]\n");
     fprintf(stderr,"   -l n  (ticket lifetime in minutes)\n");
+    fprintf(stderr,"   -K n  (check and renew tgt every n minutes, implies -q unless -v)\n"); 
+    fprintf(stderr,"   -k <ticket_file>  use ticket_file as ticket cache\n"); 
     fprintf(stderr,"   -s  read password from stdin\n");
     fprintf(stderr,"   -f <srvtab> read password from svrtab file\n");
     fprintf(stderr,"   -q  quiet\n");
