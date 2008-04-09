ecfbf253ba589c426e9126102f589eb91b21f8ac
Russ Allbery <rra@stanford.edu>
2008-04-09 22:23:08 +0000

    Fix coding style for system.h.  Add support for bool and some other portability macros.

--- portable/system.h
+++ portable/system.h
@@ -1,26 +1,33 @@
-/*  $Id$
-**
-**  Declarations of routines and variables in the C library.  Including this
-**  file is the equivalent of including all of the following headers,
-**  portably:
-**
-**      #include <sys/types.h>
-**      #include <stdarg.h>
-**      #include <stdio.h>
-**      #include <stdlib.h>
-**      #include <stddef.h>
-**      #include <string.h>
-**      #include <unistd.h>
-**
-**  Missing functions are provided via #define or prototyped if available.
-**  Also provides some standard #defines.
-*/
+/* $Id$
+ *
+ * Declarations of routines and variables in the C library.  Including this
+ * file is the equivalent of including all of the following headers, portably:
+ *
+ *     #include <sys/types.h>
+ *     #include <stdarg.h>
+ *     #include <stdbool.h>
+ *     #include <stdio.h>
+ *     #include <stdlib.h>
+ *     #include <stddef.h>
+ *     #include <stdint.h>
+ *     #include <string.h>
+ *     #include <unistd.h>
+ *
+ * Missing functions are provided via #define or prototyped if available.
+ * Also provides some standard #defines.
+ *
+ * Written by Russ Allbery <rra@stanford.edu>
+ * This work is hereby placed in the public domain by its author.
+ */
 
-#ifndef SYSTEM_H
-#define SYSTEM_H 1
+#ifndef PORTABLE_SYSTEM_H
+#define PORTABLE_SYSTEM_H 1
 
 /* Make sure we have our configuration information. */
 #include <config.h>
+
+/* BEGIN_DECL and __attribute__. */
+#include <portable/macros.h>
 
 /* A set of standard ANSI C headers.  We don't care about pre-ANSI systems. */
 #include <stdarg.h>
@@ -44,35 +51,16 @@
 # include <sys/bitypes.h>
 #endif
 
-/* __attribute__ is available in gcc 2.5 and later, but only with gcc 2.7
-   could you use the __format__ form of the attributes, which is what we use
-   (to avoid confusion with other macros). */
-#ifndef __attribute__
-# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
-#  define __attribute__(spec)   /* empty */
-# endif
-#endif
-
-/* Used for unused parameters to silence gcc warnings. */
-#define UNUSED  __attribute__((__unused__))
-
-/* BEGIN_DECLS is used at the beginning of declarations so that C++
-   compilers don't mangle their names.  END_DECLS is used at the end. */
-#undef BEGIN_DECLS
-#undef END_DECLS
-#ifdef __cplusplus
-# define BEGIN_DECLS    extern "C" {
-# define END_DECLS      }
-#else
-# define BEGIN_DECLS    /* empty */
-# define END_DECLS      /* empty */
-#endif
+/* Get the bool type. */
+#include <portable/stdbool.h>
 
 BEGIN_DECLS
 
-/* Provide prototypes for functions not declared in system headers.  Use the
-   HAVE_DECL macros for those functions that may be prototyped but
-   implemented incorrectly or implemented without a prototype. */
+/*
+ * Provide prototypes for functions not declared in system headers.  Use the
+ * HAVE_DECL macros for those functions that may be prototyped but
+ * implemented incorrectly or implemented without a prototype.
+ */
 #if !HAVE_ASPRINTF
 extern int asprintf(char **, const char *, ...);
 extern int vasprintf(char **, const char *, va_list);
@@ -93,4 +81,31 @@ extern int vsnprintf(char *, size_t, const char *, va_list);
 
 END_DECLS
 
-#endif /* !CLIBRARY_H */
+/* Windows provides snprintf under a different name. */
+#ifdef _WIN32
+# define snprintf _snprintf
+#endif
+
+/*
+ * POSIX requires that these be defined in <unistd.h>.  If one of them has
+ * been defined, all the rest almost certainly have.
+ */
+#ifndef STDIN_FILENO
+# define STDIN_FILENO   0
+# define STDOUT_FILENO  1
+# define STDERR_FILENO  2
+#endif
+
+/*
+ * C99 requires va_copy.  Older versions of GCC provide __va_copy.  Per the
+ * Autoconf manual, memcpy is a generally portable fallback.
+ */
+#ifndef va_copy
+# ifdef __va_copy
+#  define va_copy(d, s)         __va_copy((d), (s))
+# else
+#  define va_copy(d, s)         memcpy(&(d), &(s), sizeof(va_list))
+# endif
+#endif
+
+#endif /* !PORTABLE_SYSTEM_H */
